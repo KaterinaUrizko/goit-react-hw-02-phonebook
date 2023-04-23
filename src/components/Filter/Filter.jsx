052dffa677ef983f866e4b3cd2874650d02c9bd3
Katerina Urizko <93221162+KaterinaUrizko@users.noreply.github.com>
2023-04-23 18:43:29 +0300

    Homework

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import './Filter.module.css';
+
+export const Filter = ({filter, changeFilterInput})=>(
+    <label>
+        <input type="text" 
+        name={filter} 
+        onChange={changeFilterInput}
+        placeholder='Find contacts by name'
+        />
+    </label>
+);
+
+Filter.propTypes = {
+    filter: PropTypes.string,
+    changeFilterInput: PropTypes.func.isRequired
+};
